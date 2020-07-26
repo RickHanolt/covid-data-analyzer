6e0591a11803ee93e587f129e390c4b0c274200d
Richard Hanolt <rhanolt@gmail.com>
2020-07-26 09:35:40 +0000

    first commit

---
+++ lib/covid/data/analyzer.rb
@@ -0,0 +1,10 @@
+require "covid/data/analyzer/version"
+
+module Covid
+  module Data
+    module Analyzer
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/covid/data/analyzer/version.rb
@@ -0,0 +1,7 @@
+module Covid
+  module Data
+    module Analyzer
+      VERSION = "0.1.0"
+    end
+  end
+end
