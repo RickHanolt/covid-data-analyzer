e9b77a64357d760d82ba2cf47d26981970246ece
Richard Hanolt <rhanolt@gmail.com>
2020-08-01 09:53:45 +0000

    Created state_scraper method to scrape covid tracking project data

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -13,4 +13,49 @@ class Scraper
     end
   end
 
+  def testing_per_thousand
+
+  end
+
+  doc = Nokogiri::HTML(open('https://covidtracking.com/data'))
+  state_abbreviations = ["AL", "AK", "AS", "AZ", "AR", "CA", "CO", "CT", "DE", "DC", "FL", "GA", "GU", "HI", "ID", "IL", "IN",
+                          "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM",
+                           "NY", "NC", "ND", "MP", "OH", "OK", "OR", "PA", "PR", "RI", "SC", "SD", "TN", "TX", "VI", "UT", "VT",
+                            "VA", "WA", "WV", "WI", "WY"]
+  states = {}
+  doc.css("div.state-list-module--item--3j9Qs").each do |state|
+    current_state = state.css("a").first.text
+    states[:"#{current_state}"] = {}
+    counter = 0
+    state.css("td.table-module--align-left--Ya6ae").each do |data_point|
+      if counter == 0
+        states[current_state.to_sym][:cases] = data_point.text
+      elsif counter == 1
+        states[current_state.to_sym][:negative_tests] = data_point.text
+      elsif counter == 2
+        states[current_state.to_sym][:pending_tests] = data_point.text
+      elsif counter == 3
+        states[current_state.to_sym][:currently_hospitalized] = data_point.text
+      elsif counter == 4
+        states[current_state.to_sym][:cumulitive_hospitalized] = data_point.text
+      elsif counter == 5
+        states[current_state.to_sym][:current_icu] = data_point.text
+      elsif counter == 6
+        states[current_state.to_sym][:cumulitive_icu] = data_point.text
+      elsif counter == 7
+        states[current_state.to_sym][:current_ventilator] = data_point.text
+      elsif counter == 8
+        states[current_state.to_sym][:cumulitive_ventilator] = data_point.text
+      elsif counter == 9
+        states[current_state.to_sym][:recovered] = data_point.text
+      elsif counter == 10
+        states[current_state.to_sym][:deaths] = data_point.text
+      elsif counter == 11
+        states[current_state.to_sym][:total_test_results] = data_point.text
+      else
+      end
+      counter += 1
+    end
+    binding.pry
+  end
 end
