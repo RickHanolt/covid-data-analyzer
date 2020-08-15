23c5afa1f8ca9c07b3b514b9bdebac1934e09401
Richard Hanolt <rhanolt@gmail.com>
2020-08-15 08:04:51 +0000

    Updated CLI Class and get_data file to run app.

--- get_data.gemspec
+++ get_data.gemspec
@@ -1,7 +1,7 @@
 
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "covid/data/analyzer/version"
+require "bin/get_data"
 
 Gem::Specification.new do |spec|
   spec.name          = "covid-data-analyzer"
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,26 @@
 require 'pry'
 
 class CLI
-  
+
+  def self.welcome
+    get_data = ""
+    puts "Welcome! What would you like to do?"
+    while get_data == ""
+      puts "1. Get data for a state"
+      puts "2. Exit"
+      get_data = gets.strip
+    end
+
+    case get_data
+    when "1"
+      puts "What state would you like data for?"
+      get_data = gets.strip
+      puts State.new(get_data).data
+      welcome
+    when "2"
+      return
+    else
+      welcome
+    end
+  end
 end
