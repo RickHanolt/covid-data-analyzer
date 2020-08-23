4cb9d8971ed2b5adff73dfb4e7e47c18cdafbb24
Richard Hanolt <rhanolt@gmail.com>
2020-08-23 11:04:52 +0000

    Created if statement in #welcome in CLI class to validate state entered by user.

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,61 @@
 require 'pry'
 
 class CLI
+  @@all_states = [["AK", "Alaska"],
+                ["AL", "Alabama"],
+                ["AR", "Arkansas"],
+                ["AS", "American Samoa"],
+                ["AZ", "Arizona"],
+                ["CA", "California"],
+                ["CO", "Colorado"],
+                ["CT", "Connecticut"],
+                ["DC", "District of Columbia"],
+                ["DE", "Delaware"],
+                ["FL", "Florida"],
+                ["GA", "Georgia"],
+                ["GU", "Guam"],
+                ["HI", "Hawaii"],
+                ["IA", "Iowa"],
+                ["ID", "Idaho"],
+                ["IL", "Illinois"],
+                ["IN", "Indiana"],
+                ["KS", "Kansas"],
+                ["KY", "Kentucky"],
+                ["LA", "Louisiana"],
+                ["MA", "Massachusetts"],
+                ["MD", "Maryland"],
+                ["ME", "Maine"],
+                ["MI", "Michigan"],
+                ["MN", "Minnesota"],
+                ["MO", "Missouri"],
+                ["MS", "Mississippi"],
+                ["MT", "Montana"],
+                ["NC", "North Carolina"],
+                ["ND", "North Dakota"],
+                ["NE", "Nebraska"],
+                ["NH", "New Hampshire"],
+                ["NJ", "New Jersey"],
+                ["NM", "New Mexico"],
+                ["NV", "Nevada"],
+                ["NY", "New York"],
+                ["OH", "Ohio"],
+                ["OK", "Oklahoma"],
+                ["OR", "Oregon"],
+                ["PA", "Pennsylvania"],
+                ["PR", "Puerto Rico"],
+                ["RI", "Rhode Island"],
+                ["SC", "South Carolina"],
+                ["SD", "South Dakota"],
+                ["TN", "Tennessee"],
+                ["TX", "Texas"],
+                ["UT", "Utah"],
+                ["VA", "Virginia"],
+                ["VI", "Virgin Islands"],
+                ["VT", "Vermont"],
+                ["WA", "Washington"],
+                ["WI", "Wisconsin"],
+                ["WV", "West Virginia"],
+                ["WY", "Wyoming"] ]
 
   def welcome
     user_selection = ""
@@ -14,9 +69,13 @@ class CLI
     case user_selection
     when "1"
       puts "What state would you like data for?"
-      state_name = gets.strip
-      temp_state = State.find_or_create_state(state_name)
-      puts temp_state
+      state_name = gets.strip.capitalize
+      if @@all_states.any?{|state| state[1] == state_name}
+        temp_state = State.find_or_create_state(state_name)
+        puts "For more information, please visit #{temp_state.state_link} ."
+      else
+        puts "Invalid state name. Please enter a valid state."
+      end
       welcome
     when "2"
       return
